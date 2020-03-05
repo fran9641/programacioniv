47ea8343b26cfca0700f3db047475966c528578b
fran9641 <fran.cs9641@gmail.com>
2020-03-05 13:57:33 -0600

    Ufff

--- medidas/app.js
+++ medidas/app.js
@@ -3,13 +3,24 @@ document.addEventListener("DOMContentLoaded", e=>{
     form.addEventListener("submit", event=>{
         event.preventDefault();
   
-        let cantidad = document.querySelector("#txtCantidadConversor").value,
+        let cantidad = document.querySelector("#txtdato").value,
             ocs=document.getElementById("select").value,
             opcion = document.getElementById('cboConvertir');
             cantidad.split(",");
 
         if(ocs=="media"){
-            
+            let lista=cantidad.split(",").map(Number);
+            let max=0, val=0,defmod=0;
+            var counter={};
+            var mode=[];
+
+            for(var i in lista){
+                if(!(lista[i] in counter))
+                counter[lista[i]]=0;
+                counter[lista[i]]++;
+                if(counter[lista[i]]==max)
+                mode.push(lista[i]);
+            }
         }
 
     });
